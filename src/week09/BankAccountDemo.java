880caa2e25fc4386b993b0e125093fa55ecb232a
John Mafura <u1874295@unimail.hud.ac.uk>
2018-11-30 16:49:04 +0000

    it do be done again

--- src/week09/BankAccountDemo.java
+++ src/week09/BankAccountDemo.java
@@ -6,10 +6,26 @@ package week09;
 public class BankAccountDemo {
 
     public static void main (String[] args){
-        BankAccount b = new BankAccount("783268323", "Jane Doe", true);
-        System.out.println(b);
-        if (b.getAccountNumber()=="783268323"){
-            System.out.println("Hello"+b.getAccountHolder());
+        BankAccount jane = new BankAccount("783268323", "Jane Doe", true);
+        System.out.println(jane);
+        if (jane.getAccountNumber()=="783268323"){
+            System.out.println("Hello "+jane.getAccountHolder());
+
+
+
+            if (jane.deposit(100)){
+                System.out.println("Here is £100");
+                if (jane.addInterest(10)){
+                    System.out.println("We were feeling generous so we added 10% interest.\nYour balance is £"+jane.getBalance());
+                    System.out.println("jane = " + jane);
+                    jane.withdraw(jane.getBalance());
+                    System.out.println("Joking "+jane.getAccountHolder()+" we don't give out free money.\nYour balance is £"+jane.getBalance());
+                    System.out.println("jane = " + jane);
+                }
+            }
+
+            jane.setHasOverdraft(true);
+            System.out.println("jane = " + jane);
 
         }
     }
